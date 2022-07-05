6fc2a8b9ad3cc147a86ce0cd405ffcdc25f12b27
Priscila Franco <prisciladsfranco@gmail.com>
2022-07-05 13:06:24 -0300

    adicionando arquivos do projeto

---
+++ js/index.js
@@ -0,0 +1,9 @@
+
+const enviar = document.getElementById('submit');
+
+
+enviar.onclick = function () {
+    alert ('Formulário encaminhado com sucesso. Aguarde o retorno de nossa equipe em até 72h.')
+};
+
+
